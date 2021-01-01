8b3fa5c7392f094eaf0b3b2ea8b46bdbdbb822c4
gourab98 <gourabsahag@gmail.com>
2021-01-01 17:27:10 +0600

    Using Home Blog post templates

--- House_Renting_Solver/blog/views.py
+++ House_Renting_Solver/blog/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'owner': 'Gourab Saha',
+        'location': 'Sylhet',
+        'content': 'Hostel',
+        'date_posted': '1 December,2020'
+    },
+    {
+        'owner': 'Sourav Saha',
+        'location': 'Gopalganj',
+        'content': 'Family House',
+        'date_posted': '31 December,2020'
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1> Blog Home </h1>')
+    context ={
+        'posts': posts
+    }
+    return render (request ,'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1> Blog About </h1>')
-
- 
+    return render (request, 'blog/about.html')
