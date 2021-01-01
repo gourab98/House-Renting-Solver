531181f29d339a4ba665d35c78ef149dfaf11c5d
gourab98 <gourabsahag@gmail.com>
2021-01-01 11:55:09 +0600

    Add about  section's url

--- House_Renting_Solver/blog/views.py
+++ House_Renting_Solver/blog/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>Blog Home<\h1>')
+    return HttpResponse('<h1> Blog Home </h1>')
+
+
+def about(request):
+    return HttpResponse('<h1> Blog About </h1>')
+ 
