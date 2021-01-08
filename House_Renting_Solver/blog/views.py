e455539eb55e68f26fccc94c8768d007f5313ac2
gourab98 <gourabsahag@gmail.com>
2021-01-08 18:34:13 +0600

    Individual Post

--- House_Renting_Solver/blog/views.py
+++ House_Renting_Solver/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from django.http import HttpResponse
 from .models import Post
 
@@ -32,6 +33,17 @@ def home(request):
 
     }
     return render (request ,'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
 
 
 def about(request):
