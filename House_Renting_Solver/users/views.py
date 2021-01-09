301ca7176fa2b229613cb395b26f500dd1dd73f5
gourab98 <gourabsahag@gmail.com>
2021-01-09 23:27:40 +0600

    Update

--- House_Renting_Solver/users/views.py
+++ House_Renting_Solver/users/views.py
@@ -7,13 +7,13 @@ from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 def register(request):
     if request.method == 'POST':
             form = UserRegisterForm(request.POST)
-            if form.is_valid():
+            if  form.is_valid():
                 form.save()
                 username = form.cleaned_data.get('username')
                 messages.success(request, f'Your account has been created! You are now able to log in!')
                 return redirect('login')
     else:
-     form = UserRegisterForm()
+         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
 @login_required
